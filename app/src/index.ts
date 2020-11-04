4ef2028b4f1393c5eb990726c76768013ed0688a
Robert Mills <rmills@brainloop.com>
2020-11-04 12:50:54 -0500

    force hide .env file, used shared for getting a connection

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,19 +1,8 @@
-import { createConnection, In } from 'typeorm'
-import { ConnectionOptions } from 'typeorm/connection/ConnectionOptions'
-import * as path from 'path'
-import { User } from 'typeorm-shared'
-
-const root = path.resolve(__dirname, '..')
-const options: ConnectionOptions = {
-    type: 'sqlite',
-    database: `${root}/data/sample.sqlite`,
-    synchronize: true,
-    logging: true,
-    entities: [User]
-}
+import { In } from 'typeorm'
+import { getOrCreateConnection, User } from 'typeorm-shared'
 
 async function start () {
-    const connection = await createConnection(options)
+    const connection = await getOrCreateConnection('someone')
     const repository = connection.getRepository(User)
     const users = await repository.find({ id: In(['00f9955c-49e1-4391-b4f8-e4c0bd1d0ce2', '0d3a54cd-116b-4db5-8288-4e8d1ee21946'])})
     console.log(users)

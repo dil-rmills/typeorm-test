d09ec393143ea53fd318f4138a70326def109b81
Robert Mills <rmills@brainloop.com>
2020-11-04 13:35:06 -0500

    if we are going to log, we might as well log!

--- shared/src/database/repositories/user.repository.ts
+++ shared/src/database/repositories/user.repository.ts
@@ -6,4 +6,5 @@ export const getUsers = async () => {
     const connection = await getOrCreateConnection('someone')
     const repository = connection.getRepository(User)
     const users = await repository.find({ id: In(['00f9955c-49e1-4391-b4f8-e4c0bd1d0ce2', '0d3a54cd-116b-4db5-8288-4e8d1ee21946'])})
+    console.log(users)
 }
